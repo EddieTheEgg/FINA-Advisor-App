c5b6c9384e7dcdab0b0dd599bb25c74fe6fa31c8
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-05-06 17:07:12 -0400

    Updated the user table in db so that we can also verify identity for future secure financial information

--- backend/src/entities/user.py
+++ backend/src/entities/user.py
@@ -13,6 +13,8 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     username = Column(String, nullable=False)
+    first_name = Column(String, nullable = False)
+    last_name = Column(String, nullable = False)
     is_active_account = Column(Boolean, default=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
