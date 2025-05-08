a671fca548d440428d2d8a20a088994bf2667812
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-05-07 21:48:29 -0400

    Setup common user routes and services

--- backend/src/users/controller.py
+++ backend/src/users/controller.py
@@ -1,17 +1,25 @@
 from fastapi import APIRouter
+from starlette import status
+from backend.src.auth.service import CurrentUser
+from backend.src.database.core import DbSession
+from backend.src.users.model import UserResponse, PasswordChange
+from backend.src.users import service
 
 router = APIRouter(
     prefix='/users',
     tags=['users']
 )
 
-@router.get("/")
-def get_users():
-    # Some implementation
-    return {"message": "List of users"}
+@router.get("/me", response_model = UserResponse)
+def get_current_user( db: DbSession, current_user: CurrentUser):
+    return service.get_user_by_id(db, current_user.get_uuid())
 
-@router.post("/")
-def create_user():
-    # Some implementation
-    return {"message": "User created"}
+@router.put("/change-password", status_code = status.HTTP_200_OK)
+def change_password(
+    db: DbSession,
+    current_user: CurrentUser,
+    password_change: PasswordChange
+):
+    service.change_password(db, current_user.get_uuid(), password_change)
+
 
--- backend/src/users/service.py
+++ backend/src/users/service.py
@@ -1,1 +1,37 @@
-#Actual business logic will be implemented here
+import logging
+from uuid import UUID
+from sqlalchemy.orm import Session
+from backend.src.auth.service import verify_password, get_password_hash
+from backend.src.users.model import UserResponse, PasswordChange
+from backend.src.entities.user import User
+from backend.src.exceptions import UserNotFoundError, InvalidPasswordError, PasswordMismatchError
+
+def get_user_by_id(db: Session, user_id: UUID) -> UserResponse:
+    user = db.query(User).filter(user_id == User.user_id).first()
+    if not user:
+        logging.warning(f"User not found with given ID: {user_id}")
+        raise UserNotFoundError(user_id)
+    logging.info(f"Succesfully retrieved user with ID: {user_id}")
+    return user
+
+def change_password(db: Session, user_id: UUID, password_change: PasswordChange):
+    try:
+        user = get_user_by_id(db, user_id)
+
+        #Verify current password (before change) is valid
+        if not verify_password(password_change.current_password, user.hashed_password):
+            logging.warning(f"Current password provided for user ID: {user_id} was invalid")
+            raise InvalidPasswordError()
+        
+        #Verify new passwords match each other
+        if password_change.new_password != password_change.new_password_confirm:
+            logging.warning(f"Password mismatch during change attempt for user ID")
+            raise PasswordMismatchError()
+
+        user.hashed_password = get_password_hash(password_change.new_password)
+        db.commit()
+        logging.info(f"Successfully changed password for user ID: {user_id}")
+    except Exception as e:
+        logging.error(f"Error during password change for user ID: {user_id}. Error: {str(e)}")
+        raise
+        
