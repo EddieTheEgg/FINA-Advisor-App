45129228934cadf08c48dce6e706bdc8c45a99ea
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-05-08 23:20:21 -0400

    added category services, user, and updated the db columns to reflect service

---
+++ backend/src/categories/model.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel
+
+class CreateCategoryRequest(BaseModel):
+    category_name: str
+    icon: str | None = None
+    color: str
+    is_income: bool = False
+    is_custom: bool = False
+
+class CategoryUpdate(BaseModel):
+    category_name: str
+    icon: str | None = None
+    color: str | None = None
+    is_income: bool | None = None
+
+class CategoryResponse(BaseModel):
+    category_id: UUID
+    category_name: str
+    icon: str | None = None
+    color: str
+    is_income: bool
+    is_custom: bool
+    user_id: UUID | None = None
+    created_at: datetime
+    updated_at: datetime | None = None
+
+    class Config:
+        from_attributes = True
+        arbitrary_types_allowed = True
+
+    
--- backend/src/categories/service.py
+++ backend/src/categories/service.py
@@ -1,1 +1,45 @@
-#Actual business logic will be implemented here
+import logging
+from typing import List
+from uuid import UUID
+from backend.src.categories.model import CreateCategoryRequest
+from sqlalchemy.orm import Session
+
+from backend.src.entities.category import Category
+
+# This is when a user makes a new category besides the default ones, which is custom
+def create_category(db: Session, create_category_request: CreateCategoryRequest, user_id: UUID) -> Category:
+    try:
+        new_category = Category(
+            category_name=create_category_request.category_name,
+            icon=create_category_request.icon,
+            color=create_category_request.color,
+            is_income = create_category_request.income,
+            is_custom = True,
+            user_id = user_id
+        )
+        db.add(new_category)
+        db.commit()
+        db.refresh(new_category)
+        return new_category
+    except Exception as e:
+        logging.error(f"Failed to create new category: {create_category_request.category_name}. Error {str(e)}")
+        raise
+
+# This gets "all" categories connected to the user
+def get_user_categories(
+        db: Session,
+        user_id: UUID,
+        skip: int = 0,
+        limit: int = 100,
+        include_system_default_categories: bool = True,
+) -> List[Category]:
+    possibleCategories = db.query(Category)
+
+    #Gets all default and user's possible categories to work with if true
+    # else only gets the user self-created categories
+    if include_system_default_categories:
+        possibleCategories = possibleCategories.filter(
+            (Category.user_id == user_id) | ((Category.is_custom == False) & (Category.user_id == user_id)))
+    else:
+        possibleCategories = possibleCategories.filter((Category.user_id == user_id) & (Category.is_custom == True))
+    return possibleCategories.offset(skip).limit(limit).all()
