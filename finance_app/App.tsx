49880477c469b5bd63c460e6cb09b6e02e3fd562
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-05-29 21:22:26 -0400

    Added loading screen function whenever an await type function is being called

--- finance_app/App.tsx
+++ finance_app/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './src/navigation/AuthNavigator.tsx';
+import { AuthProvider } from './src/features/auth/context/AuthContext.tsx';
+import { HomeNavigator } from './src/navigation/HomeNavigator.tsx';
+import { useAuth } from './src/features/auth/hooks/useAuth.tsx';
+import LoadingScreen from './src/components/LoadingScreen/LoadingScreen.tsx';
+
+const AppContent = () => {
+  const {isSignedIn, isLoading} = useAuth();
+
+// show a loading state while checking the auth status
+
+  if (isLoading || isSignedIn === null) {
+    return <LoadingScreen />;
+  }
+
+  return (
+      <NavigationContainer>
+        {isSignedIn ? <HomeNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+  );
+};
 
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AuthNavigator />
-    </NavigationContainer>
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
   );
 };
 
