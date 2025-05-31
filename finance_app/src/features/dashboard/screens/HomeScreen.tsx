2c072a5f5214c6e915811753c68f14925cfe4528
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-05-31 00:09:39 -0400

    Small test template of how api fetching with axios using the new header singleton implementation

--- finance_app/src/features/dashboard/screens/HomeScreen.tsx
+++ finance_app/src/features/dashboard/screens/HomeScreen.tsx
@@ -1,11 +1,29 @@
 import { View, Text, SafeAreaView } from 'react-native';
 import { SignOutButton } from '../../auth/components/SignOutButton/SignOutButton';
+import { useEffect, useState, useCallback } from 'react';
+import api from '../../../api/axios';
 
 export const HomeScreen = () => {
+    const [username, setUsername] = useState('');
+
+    const getUsername = useCallback(async() => {
+        try{
+            const response = await api.get('/users/me');
+            setUsername(response.data.first_name);
+        } catch (error) {
+            console.error('Error fetching username:', error);
+        }
+    }, []);
+
+    useEffect(() => {
+        getUsername();
+    }, [getUsername]);
+
     return (
         <SafeAreaView>
             <View>
                 <Text>Home</Text>
+                <Text>{username}</Text>
                 {/* <SignOutButton />  For testing purposes*/}
             </View>
         </SafeAreaView>
