a8003ef8c645f904a0d83dda5f1771d5ebf53471
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-06-01 22:40:18 -0400

    Added the query client wrapper for the app

--- finance_app/App.tsx
+++ finance_app/App.tsx
@@ -5,12 +5,12 @@ import { AuthProvider } from './src/features/auth/context/AuthContext.tsx';
 import { HomeNavigator } from './src/navigation/HomeNavigator.tsx';
 import { useAuth } from './src/features/auth/hooks/useAuth.tsx';
 import LoadingScreen from './src/components/LoadingScreen/LoadingScreen.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const AppContent = () => {
   const {isSignedIn, isLoading} = useAuth();
 
 // show a loading state while checking the auth status
-
   if (isLoading || isSignedIn === null) {
     return <LoadingScreen />;
   }
@@ -24,10 +24,14 @@ const AppContent = () => {
 
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <AuthProvider>
-      <AppContent />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppContent />
+      </AuthProvider>
+    </QueryClientProvider>
   );
 };
 
