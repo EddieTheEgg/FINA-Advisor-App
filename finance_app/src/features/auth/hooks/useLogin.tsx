b858cfbaf4310ace4df8ac7cf804747916ec733b
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-06-01 23:17:26 -0400

    store sign in token moved from authcontext to useLogin hook to keep logic more organized

--- finance_app/src/features/auth/hooks/useLogin.tsx
+++ finance_app/src/features/auth/hooks/useLogin.tsx
@@ -1,9 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { loginUser } from '../api/api';
+import { useAuth } from './useAuth';
+import { Token } from '../types';
 
 export const useLogin = () => {
+    const { signInTokens } = useAuth();
     return useMutation({
         mutationFn: loginUser,
+        onSuccess: (data: Token) => {
+            console.log('Login successful:', data);
+            signInTokens(data.accessToken, data.refreshToken);
+        },
         onError: (error: any) => {
             console.error('Login failed:', error);
             return {
