af5f1648d4fa6efe392cec801d37cf36b3a8056f
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-06-23 18:25:34 -0400

    Removed transaction screen for account, going to be put with the account details screen

--- finance_app/src/features/accounts/screens/AccountTransactionsScreen/AccountTransactionScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export const AccountTransactionsScreen = () => {
-    return (
-        <View>
-            <Text>Account Transactions</Text>
-        </View>
-    );
-};
