480484f4eb2a5ac487a11610d05a2747f3492f60
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-07-01 00:01:11 -0400

    Minor animation addded to selecting account (which we will implement to selection screen soon)

--- finance_app/src/features/accounts/screens/TransferScreen/TransferScreen.tsx
+++ finance_app/src/features/accounts/screens/TransferScreen/TransferScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, useWindowDimensions, ScrollView, Pressable } from 'react-native';
+import { View, Text, useWindowDimensions, ScrollView, Pressable, Animated } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useRoute } from '@react-navigation/native';
 import { AccountNavigatorParamList } from '../../../../navigation/types/AccountNavigatorTypes';
@@ -6,16 +6,39 @@ import { RouteProp } from '@react-navigation/native';
 import { styles } from './TransferScreen.styles';
 import GoBackButton from '../../../auth/components/GoBackButton/GoBackButton';
 import { TransferAccountCard } from '../../components/TransferAccountCard/TransferAccountCard';
+import { useRef } from 'react';
 
 
 export const TransferScreen = () => {
     const { fromAccountDetails, toAccountDetails } = useRoute<RouteProp<AccountNavigatorParamList, 'Transfer'>>().params;
 
     const insets = useSafeAreaInsets();
-
     const dimensions = useWindowDimensions();
     const { height } = dimensions;
     const responsivePaddingTop = insets.top + (height * 0.02);
+
+    const animation = useRef(new Animated.Value(0)).current;
+    const scale = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [1,0.9],
+    });
+
+    const onPressIn = () => {
+        Animated.spring(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    const onPressOut = () => {
+        setTimeout(() => {
+            Animated.spring(animation, {
+                toValue: 0,
+                useNativeDriver: true,
+            }).start();
+        }, 200); // 200ms delay to ensure the animation is smooth
+    };
+
 
     return (
         <View style = {[styles.transferScreenContainer,{paddingTop: responsivePaddingTop}]}>
@@ -26,19 +49,23 @@ export const TransferScreen = () => {
             <ScrollView>
                 <View style = {styles.fromAccountContainer}>
                     <Text style = {styles.fromAccountTitle}>From</Text>
-                    <Pressable>
-                        {fromAccountDetails ? (
-                           <TransferAccountCard
-                                emptyCard={false}
-                                accountColor = {fromAccountDetails.color}
-                                accountIcon = {fromAccountDetails.icon ?? undefined}
-                                accountBalance = {fromAccountDetails.balance}
-                                accountName = {fromAccountDetails.name}
-                           />
-                        ) : (
-                           <TransferAccountCard emptyCard={true} />
-                        )}
-                    </Pressable>
+                    <Animated.View style = {{transform: [{scale}]}}>
+                        <Pressable
+                            onPressIn={onPressIn}
+                            onPressOut={onPressOut}>
+                            {fromAccountDetails ? (
+                            <TransferAccountCard
+                                    emptyCard={false}
+                                    accountColor = {fromAccountDetails.color}
+                                    accountIcon = {fromAccountDetails.icon ?? undefined}
+                                    accountBalance = {fromAccountDetails.balance}
+                                    accountName = {fromAccountDetails.name}
+                            />
+                            ) : (
+                            <TransferAccountCard emptyCard={true} />
+                            )}
+                        </Pressable>
+                    </Animated.View>
                 </View>
             </ScrollView>
         </View>
