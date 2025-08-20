a0a04784f1075e76cf38a4fe55191094ced15dda
Edison Kwok <150745631+EddieTheEgg@users.noreply.github.com>
2025-08-19 22:41:04 -0400

    Setup edit category icon component

---
+++ finance_app/src/features/settings/components/EditCategoryIcon/EditCategoryIcon.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export const EditCategoryIcon = () => {
+    return (
+        <View>
+            <Text>Category Icon</Text>
+        </View>
+    );
+};
